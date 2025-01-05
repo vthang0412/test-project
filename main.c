cfef708928dd89e4fd45e7e1ef15d4c6c4a11205
vthang0412 <vthang041206@gmail.com>
2025-01-05 23:24:33 +0700

    Update main.c

--- main.c
+++ main.c
@@ -21,7 +21,55 @@ int main(int argc, char *argv[]) {
 		case 3:
 		
 	}
-	
+	void searchStudentByName(Student students[], int *length) {
+    if (*length == 0) {
+        printf("Danh sach sinh vien hien dang trong. Khong co gi de tim kiem.\n");
+        return;
+    }
+    char searchName[50];
+    clearInputBuffer();
+
+    printf("\t **** Search A Student ****\n");
+    printf("Nhap ten sinh vien muon tim kiem: ");
+    fgets(searchName, sizeof(searchName), stdin);
+    searchName[strcspn(searchName, "\n")] = '\0'; 
+
+    
+    if (strlen(searchName) == 0) {
+        printf("Ten sinh vien khong duoc de trong. Vui long nhap lai.\n");
+        return searchStudentByName(students, length);
+    }
+
+    printf("\nKet qua tim kiem cho chuoi: \"%s\"\n", searchName);
+    int found = 0;
+    int i; 
+    for (i = 0; i < *length; i++) {
+        if (strstr(students[i].name, searchName) == 0) {
+//            if (!found) {
+//                found = 1;
+                printf("\t----------***All Students***----------\n");
+                printf("|==========|====================|========================|====================|============|\n");
+                printf("|    ID    |        Name        |          Email         |        Phone       |  NO.Course |\n");
+                printf("|==========|====================|========================|====================|============|\n");
+//            }
+			found=1;
+            printf("| %-8d | %-18s | %-22s | %-18s | %-10d |\n", 
+                   students[i].studentId, 
+                   students[i].name, 
+                   students[i].email, 
+                   students[i].phone, 
+                   students[i].courses);
+        }
+    }
+
+    if (found!=1) {
+        printf("|==========|====================|========================|====================|============|\n");
+    } else {
+        printf("Khong tim thay sinh vien nao voi ten chua chuoi: \"%s\"\n", searchName);
+    }
+    printf("\t \t---------------------\n");
+}
+
 	
 
 		
